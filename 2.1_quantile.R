7dfdd9c36a9d66093fcb7c4690c916318e00d264
ml4economics <karsten.opitz@studium.fernuni-hagen.de>
2025-01-09 14:40:07 +0100

    code cleanup

--- 2.1_quantile.R
+++ 2.1_quantile.R
@@ -1,27 +1,33 @@
 source("util.R")
 
 use_gamma <- FALSE
-p = 0.3
+p <- 0.3
 
 if (use_gamma == TRUE) {
-  shape = 1
-  scale = 2
-  x_low = 0
-  x_high = 5
+  shape <- 1
+  scale <- 2
+  x_low <- 0
+  x_high <- 5
   pfunc <- partial_func(pgamma, shape = shape, scale = scale)
   qfunc <- partial_func(qgamma, shape = shape, scale = scale)
-  distribution = sprintf("Γ(%d,%d)-Verteilung", shape, scale)
+  distribution <- sprintf("Γ(%d,%d)-Verteilung", shape, scale)
 } else {
-  df = 2
-  x_low = -2
-  x_high = 2
+  df <- 2
+  x_low <- -2
+  x_high <- 2
   pfunc <- partial_func(pt, df = df)
   qfunc <- partial_func(qt, df = df)
-  distribution = sprintf("t-Verteilung (df=%d)", df)
+  distribution <- sprintf("t-Verteilung (df=%d)", df)
 }
 
-plot <- cdf_with_quantile_plot(pfunc, qfunc, x_low, x_high, p, NULL, label_size = 6) +
-        theme(aspect.ratio=0.7) + theme_text(axis_title=20) + coord_cartesian(clip = "off")
+plot <- cdf_with_quantile_plot(pfunc, qfunc, 
+                               x_low, x_high, 
+                               p, 
+                               NULL, 
+                               label_size = 6) +
+        theme(aspect.ratio=0.7) + 
+        theme_text(axis_title=20) + 
+        coord_cartesian(clip = "off")
 
 # Display the plot
 print(plot)
